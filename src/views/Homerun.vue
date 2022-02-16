44206e0f657c6969af91e2583ffe1b634bb91652
Chi Yang <dddenrose@gmail.com>
2022-02-16 14:25:54 +0800

    首頁架構完成

--- src/views/Homerun.vue
+++ src/views/Homerun.vue
@@ -1,6 +1,254 @@
 <template>
   <div class="hello">
-    
+    <div class="header">
+      <div class="nav">
+        <div class="container">
+          <div class="top-bar">
+            <div class="bar-1">
+            <a href="#" class="shopping">shopping list</a>
+            <a href="#">about us</a>
+            <a href="#">contact</a>
+            <a href="#">social media</a>
+            </div>
+            <div class="bar-2">login</div>
+          </div>
+          <div class="logo"><img src="../img/logo.png" alt="logo" style="width:100px"></div>
+          <div class="nav">
+            <a href="#">MEN'S WATCHES</a>
+            <a href="#">LADIES' WATCHES</a>
+            <a href="#">NEW RELEASES</a>
+            <a href="#">ACCESSORIES</a>
+            <a href="#">OTHER</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="main-photo">
+        <div class="photo-top">
+          <img src="../img/s11.jpg" alt="image" style="width:100%">
+          <div class="info">
+            <h3>MAN ACCESSORIES</h3>
+            <h1>ALWAYS BE A GENTLEMAN</h1>
+            <h4>Cras magna nunc, tempus in odio eu, lacinia placerat tellus. In rutrum iaculis elit, sed sodales lacus ullamcorper ut. Nunc venenatis sempe</h4>
+          </div>
+        </div>
+        <div class="photo-row">
+          <img src="../img/s12.jpg" alt="image" style="width:100%">
+          <img src="../img/s01.jpg" alt="image" style="width:100%">
+          <img src="../img/s06.jpg" alt="image" style="width:100%">
+        </div>
+      </div>
+      <div class="trending-products">
+        <div class="container">
+          <div class="title topic">
+            <div class="line"></div>
+            <div class="info">
+              <h2>TRENDING PRODUCTS</h2>
+              <h4>Nam interdum, lacus quis tempor iaculis, nunc mauris tincidunt metus,</h4>
+            </div>
+            <div class="line"></div>
+          </div>
+          <div class="product-bar">
+            <div class="product">
+              <img src="../img/p01.jpg" alt="prodcut">
+              <h2>1815</h2>
+              <h3 class="name">JUSTIN GATILIN</h3>
+              <h3>GOLD/BLUE TURTLE</h3>
+              <h3>$10,100</h3>
+            </div>
+            <div class="product">
+              <img src="../img/p13.jpg" alt="prodcut">
+              <h2>1815</h2>
+              <h3 class="name">CHRONOGRAPH</h3>
+              <h3>STEEL / BLACK</h3>
+              <h3>$9,500</h3>
+            </div>
+            <div class="product">
+              <img src="../img/p12.jpg" alt="prodcut">
+              <h2>1815</h2>
+              <h3 class="name">AT' SEA</h3>
+              <h3>GOLD/BLUE TURTLE</h3>
+              <h3>$12,500</h3>
+            </div>
+            <div class="product">
+              <img src="../img/p11.jpg" alt="prodcut">
+              <h2>1815</h2>
+              <h3 class="name">CHRONOGRAPH</h3>
+              <h3>STEEL / BLACK</h3>
+              <h3>$9,500</h3>
+            </div>
+          </div>
+          <div class="product-bar">
+            <div class="product">
+              <img src="../img/p10.jpg" alt="prodcut">
+              <h2>1982</h2>
+              <h3 class="name">JUSTIN GATILIN</h3>
+              <h3>GOLD/BLUE TURTLE</h3>
+              <h3>$10,100</h3>
+            </div>
+            <div class="product">
+              <img src="../img/p09.jpg" alt="prodcut">
+              <h2>1815</h2>
+              <h3 class="name">AT' SEA</h3>
+              <h3>GOLD/BLUE TURTLE</h3>
+              <h3>$13,500</h3>
+            </div>
+            <div class="product">
+              <img src="../img/p08.jpg" alt="prodcut">
+              <h2>1982</h2>
+              <h3 class="name">JUSTIN GATILIN</h3>
+              <h3>GOLD/BLUE TURTLE</h3>
+              <h3>$15,500</h3>
+            </div>
+            <div class="product">
+              <img src="../img/p07.jpg" alt="prodcut">
+              <h2>1815</h2>
+              <h3 class="name">JUSTIN GATILIN</h3>
+              <h3>GOLD/GREEN TURTLE</h3>
+              <h3>$19,500</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="photo-bar">
+        <div class="container">
+          <div class="slide">
+            <div class="arrow"></div>
+            <div class="photo-row">
+              <img src="../img/s10.jpg" alt="image">
+            </div>
+            <div class="arrow"></div>
+          </div>
+          <div class="info topic">
+            <div class="title">Time Discovered Truth.</div>
+            <div class="info">
+              <h3>MAN ACCESSORIES</h3>
+              <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quam, recusandae ut libero nisi qui, error natus optio, veniam ea voluptatum sapiente repellendus. Quam molestias adipisci voluptas enim illum? Ipsa dolore provident iusto sequi voluptates quasi numquam quo atque vitae.</h5>
+            </div>
+            <div class="circle">. . . . .</div>
+          </div>
+        </div>
+      </div>
+      <div class="best-selling">
+        <div class="container">
+          <div class="title topic">
+            <div class="line"></div>
+            <div class="info">
+              <h2>BEST-SELLING PRODUCTS</h2>
+              <h4>Nam interdum, lacus quis tempor iaculis, nunc mauris tincidunt metus,</h4>
+            </div>
+            <div class="line"></div>
+          </div>
+          <div class="product-bar">
+            <div class="product">
+              <img src="../img/p10.jpg" alt="prodcut">
+              <h2>1982</h2>
+              <h3 class="name">JUSTIN GATILIN</h3>
+              <h3>GOLD/BLUE TURTLE</h3>
+              <h3>$10,100</h3>
+            </div>
+            <div class="product">
+              <img src="../img/p09.jpg" alt="prodcut">
+              <h2>1815</h2>
+              <h3 class="name">AT' SEA</h3>
+              <h3>GOLD/BLUE TURTLE</h3>
+              <h3>$13,500</h3>
+            </div>
+            <div class="product">
+              <img src="../img/p08.jpg" alt="prodcut">
+              <h2>1982</h2>
+              <h3 class="name">JUSTIN GATILIN</h3>
+              <h3>GOLD/BLUE TURTLE</h3>
+              <h3>$15,500</h3>
+            </div>
+            <div class="product">
+              <img src="../img/p07.jpg" alt="prodcut">
+              <h2>1815</h2>
+              <h3 class="name">JUSTIN GATILIN</h3>
+              <h3>GOLD/GREEN TURTLE</h3>
+              <h3>$19,500</h3>
+            </div>
+          </div>
+          <div class="circle">. . . . .</div>
+        </div>
+      </div>
+      <div class="barnd-story">
+        <div class="container">
+          <img src="../img/s13.jpg" alt="image">
+          <div class="title topic">
+            <div class="line"></div>
+            <div class="info">
+              <h2>BRAND STORY</h2>
+              <h4>Nam interdum, lacus quis tempor iaculis, nunc mauris tincidunt metus,</h4>
+            </div>
+            <div class="line"></div>
+          </div>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dicta inventore sit quo aut quaerat veniam ducimus. Dolorem cumque eaque itaque, quam perspiciatis harum provident porro aut explicabo consectetur! Tempore autem nostrum temporibus eius voluptate tempora iusto, vero, officiis asperiores enim vel excepturi cumque officia voluptatibus doloremque quos, facilis doloribus?</div>
+        </div>
+      </div>
+      <div class="photo-row">
+        <div class="one">
+          <img src="../img/s04.jpg" alt="image">
+        </div>
+        <div class="two">
+          <img src="../img/s02.jpg" alt="image">
+          <img src="../img/s07.jpg" alt="image">
+        </div>
+        <div class="one">
+          <img src="../img/s08.jpg" alt="image">
+        </div>
+        <div class="two">
+          <img src="../img/s03.jpg" alt="image">
+          <img src="../img/s09.jpg" alt="image">
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="container">
+        <div class="information">
+            
+          <div class="info">
+            <img src="../img/logo.png" alt="logo">
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+          </div>
+          <div class="info">
+            <div class="footer-title">COLLECTIONS</div>
+            <a href="#">Men's Watches</a>
+            <a href="#">Ladies' Watches</a>
+            <a href="#">New Releases</a>
+          </div>
+          <div class="info">
+            <div class="footer-title">FOLLOW US</div>
+            <a href="#">Beginnings</a>
+            <a href="#">British Wachmaking</a>
+            <a href="#">Our Technologies</a>
+            <a href="#">Aviation Military</a>
+            <a href="#">Partnerships</a>
+            <a href="#">Ambassadors</a>
+            <a href="#">Awards</a>
+          </div>
+          <div class="info">
+            <div class="footer-title">SERVICE</div>
+            <a href="#">Customer Service</a>
+            <a href="#">Finance</a>
+            <a href="#">Request A Brochure</a>
+            <a href="#">Faqs</a>
+            <a href="#">Contact Us</a>
+            <a href="#">Terms And Conditions</a>
+            <a href="#">Delivery Returns</a>
+          </div>
+          <div class="info">
+            <div class="footer-title">SOCIAL MEDIA</div>
+            <a href="#">Facebook</a>
+            <a href="#">Twitter</a>
+            <a href="#">Insagram</a>
+            <a href="#">Youtube</a>
+          </div>
+        </div>
+        <div>Bremont Watch Company Limited is registered in England 05414485. Registered office; Bremont Watch Company, Manufacturing and Technology Centre, The Wing, Reading Road, Henley-on-Thames, Oxfordshire, RG9 4GE.</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +257,10 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+//全域設定
+img {
+  vertical-align: top;
+}
 h3 {
   margin: 40px 0 0;
 }
