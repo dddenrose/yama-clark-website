68a13b7e2e1664791d23be2e2c7c7b1390842f80
Chi Yang <dddenrose@gmail.com>
2022-04-21 19:00:18 +0800

    img fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Homerun from '../views/Homerun.vue'
-import ProductList from '../views/ProductList.vue'
-import ProductDetail from '../views/ProductDetail.vue'
-import ShoppingList from '../views/ShoppingList.vue'
-import OrderList from '../views/OrderList.vue'
-import Login from '../views/Login.vue'
-import SignUpPage from '../components/SignUpPage.vue'
-import LoginPage from '../components/LoginPage.vue'
-import Home from '../views/Home.vue'
-import SignUp from '../views/SignUp.vue'
-import OrderHistory from '../views/OrderHistory.vue'
-import Profile from '../views/Profile.vue'
+// import Homerun from '../views/Homerun.vue'
+// import ProductList from '../views/ProductList.vue'
+// import ProductDetail from '../views/ProductDetail.vue'
+// import ShoppingList from '../views/ShoppingList.vue'
+// import OrderList from '../views/OrderList.vue'
+// import Login from '../views/Login.vue'
+// import SignUpPage from '../components/SignUpPage.vue'
+// import LoginPage from '../components/LoginPage.vue'
+// import Home from '../views/Home.vue'
+// import SignUp from '../views/SignUp.vue'
+// import OrderHistory from '../views/OrderHistory.vue'
+// import Profile from '../views/Profile.vue'
 // import store from '@/store/index.js'
 
 
@@ -21,65 +21,56 @@ const routes = [
   {
     path: '/',
     name: 'homerun',
-    component: Homerun,
+    // component: Homerun,
+    component: () => import(/* webpackChunkName: "Homerun" */ '../views/Homerun.vue')
   },
   {
     path: '/productlist',
     name: 'productlist',
-    component: ProductList,
+    // component: ProductList,
+    component: () => import(/* webpackChunkName: "ProductList" */ '../views/ProductList.vue')
   },
   {
     path: '/productdetail/:id',
     name: 'productdetail',
-    component: ProductDetail,
+    // component: ProductDetail,
+    component: () => import(/* webpackChunkName: "ProductDetail" */ '../views/ProductDetail.vue')
   },
   {
     path: '/shoppinglist',
     name: 'shoppinglist',
-    component: ShoppingList,
-    meta: {
-      isUserAuth: true
-    }
+    // component: ShoppingList,
+    component: () => import(/* webpackChunkName: "ShoppingList" */ '../views/ShoppingList.vue')
   },
   {
     path: '/orderlist',
     name: 'orderlist',
-    component: OrderList,
+    // component: OrderList,
+    component: () => import(/* webpackChunkName: "OrderList" */ '../views/OrderList.vue')
   },
   {
     path: '/login',
     name: 'login',
-    component: Login,
-  },
-  {
-    path: '/signuppage',
-    name: 'signuppage',
-    component: SignUpPage,
-  },
-  {
-    path: '/loginpage',
-    name: 'loginpage',
-    component: LoginPage,
-  },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home,
+    // component: Login,
+    component: () => import(/* webpackChunkName: "Login" */ '../views/Login.vue')
   },
   {
     path: '/signup',
     name: 'signup',
-    component: SignUp,
+    // component: SignUp,
+    component: () => import(/* webpackChunkName: "SignUp" */ '../views/SignUp.vue')
   },
   {
     path: '/orderhistory',
     name: 'orderhistory',
-    component: OrderHistory,
+    // component: OrderHistory,
+    component: () => import(/* webpackChunkName: "OrderHistory" */ '../views/OrderHistory.vue')
   },
   {
     path: '/profile',
     name: 'profile',
-    component: Profile,
+    // component: Profile,
+    component: () => import(/* webpackChunkName: "Profile" */ '../views/Profile.vue')
   }
 ]
 
